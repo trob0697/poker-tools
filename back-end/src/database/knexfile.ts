bd826b06dcfe6250bf37c5c1c3251d1ded0582c9
trob0697 <tahreak.robinson@gmail.com>
2022-11-26 15:34:08 -0500

    setup postgres docker

--- back-end/src/database/knexfile.ts
+++ back-end/src/database/knexfile.ts
@@ -3,7 +3,7 @@ import type { Knex } from "knex";
 export const config: { [key: string]: Knex.Config } = {
     development: {
         client: "postgresql",
-        connection: "postgresql://tahreak@localhost:5432/ptdb", // TODO: Fix this. Var not going through -> process.env.DB_CONNECTION_STRING,
+        connection: process.env.DB_CONNECTION_STRING,
         pool: {
             min: 2,
             max: 10
